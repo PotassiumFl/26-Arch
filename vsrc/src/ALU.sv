de7bc77d46fbdb8946b31b589669b7b6603d6a88
顾开孚 <3390945494@qq.com>
2026-03-13 22:47:51 +0800

    revise ALU

--- vsrc/src/ALU.sv
+++ vsrc/src/ALU.sv
@@ -6,22 +6,44 @@
 `endif
 
 module ALU import common::*; (
-    input   i1          clk,
-    input   ALU_ctrl_t  ALU_ctrl,
-    input   i64         operand,operand2,
-    output  i64         result,
-    output  i1          branch_index
+    input   i1          clk,reset,
+    
+    input   ID_EX_t     id_ex,
+    output  EX_MEM_t    ex_mem
 );
 
-    always_comb begin : ALU
-        case (ALU_ctrl.opr)
-            ADD  : result = operand + operand2;
-            SUB  : result = operand - operand2;
-            XOR  : result = operand ^ operand2;
-            OR   : result = operand | operand2;
-            AND  : result = operand & operand2;
-            default: result = 0;
+    EX_MEM_t ex_mem_next;
+    i64 result_tmp;
+
+    always_comb begin : ALU_ex
+        case(id_ex.ALU_ctrl.opr)
+            ADD: result_tmp = id_ex.ALU_ctrl.operand + id_ex.ALU_ctrl.operand2;
+            SUB: result_tmp = id_ex.ALU_ctrl.operand - id_ex.ALU_ctrl.operand2;
+            XOR: result_tmp = id_ex.ALU_ctrl.operand ^ id_ex.ALU_ctrl.operand2;
+            OR : result_tmp = id_ex.ALU_ctrl.operand | id_ex.ALU_ctrl.operand2;
+            AND: result_tmp = id_ex.ALU_ctrl.operand & id_ex.ALU_ctrl.operand2;
+            default: result_tmp = 64'b0;
         endcase
+    end
+
+    always_comb begin : word_ex
+        if(id_ex.ALU_ctrl.word_index == WORD)
+            ex_mem_next.alu_result =
+                {{32{result_tmp[31]}},result_tmp[31:0]};
+        else
+            ex_mem_next.alu_result = result_tmp;
+    end
+
+    assign ex_mem_next.rd        = id_ex.rd;
+    assign ex_mem_next.reg_write = id_ex.reg_write;
+
+    always_ff @(posedge clk or posedge reset) begin
+
+        if(reset)
+            ex_mem <= '0;
+        else
+            ex_mem <= ex_mem_next;
+
     end
     
 endmodule
