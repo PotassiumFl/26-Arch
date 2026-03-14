ceb33b6d1610347337bacbdeb51c1d79ca70cc1f
顾开孚 <3390945494@qq.com>
2026-03-14 13:19:41 +0800

    revise ALU

--- vsrc/src/ALU.sv
+++ vsrc/src/ALU.sv
@@ -7,21 +7,53 @@
 
 module ALU import common::*; (
     input   i1          clk,reset,
-    
     input   ID_EX_t     id_ex,
+    input   MEM_WB_t    mem_wb,
+    input   logic       stall,
     output  EX_MEM_t    ex_mem
 );
 
     EX_MEM_t ex_mem_next;
     i64 result_tmp;
 
+    u2 forwardA;
+    u2 forwardB;
+
+    i64 operandA;
+    i64 operandB;
+
+    Forward forward(
+        .id_ex(id_ex),
+        .ex_mem(ex_mem),
+        .mem_wb(mem_wb),
+        .forwardA(forwardA),
+        .forwardB(forwardB)
+    );
+
+    always_comb begin : forward1
+        case(forwardA)
+            2'b00: operandA = id_ex.ALU_ctrl.operand;
+            2'b10: operandA = ex_mem.alu_result;   
+            2'b01: operandA = mem_wb.result;
+            default: operandA = id_ex.ALU_ctrl.operand;
+        endcase
+    end
+    always_comb begin : forward2
+        case(forwardB)
+            2'b00: operandB = id_ex.ALU_ctrl.operand2;
+            2'b10: operandB = ex_mem.alu_result;
+            2'b01: operandB = mem_wb.result;
+            default: operandB = id_ex.ALU_ctrl.operand2;
+        endcase
+    end
+
     always_comb begin : ALU_ex
         case(id_ex.ALU_ctrl.opr)
-            ADD: result_tmp = id_ex.ALU_ctrl.operand + id_ex.ALU_ctrl.operand2;
-            SUB: result_tmp = id_ex.ALU_ctrl.operand - id_ex.ALU_ctrl.operand2;
-            XOR: result_tmp = id_ex.ALU_ctrl.operand ^ id_ex.ALU_ctrl.operand2;
-            OR : result_tmp = id_ex.ALU_ctrl.operand | id_ex.ALU_ctrl.operand2;
-            AND: result_tmp = id_ex.ALU_ctrl.operand & id_ex.ALU_ctrl.operand2;
+            ADD: result_tmp = operandA + operandB;
+            SUB: result_tmp = operandA - operandB;
+            XOR: result_tmp = operandA ^ operandB;
+            OR : result_tmp = operandA | operandB;
+            AND: result_tmp = operandA & operandB;
             default: result_tmp = 64'b0;
         endcase
     end
@@ -34,17 +66,17 @@ module ALU import common::*; (
             ex_mem_next.alu_result = result_tmp;
     end
 
-    assign ex_mem_next.rd        = id_ex.rd;
+    assign ex_mem_next.wd        = id_ex.wd;
     assign ex_mem_next.reg_write = id_ex.reg_write;
+    assign ex_mem_next.decoder_ctrl = id_ex.decoder_ctrl;
+    assign ex_mem_next.valid     = id_ex.valid;
 
-    always_ff @(posedge clk or posedge reset) begin
-
+    always_ff @(posedge clk or posedge reset) begin 
         if(reset)
             ex_mem <= '0;
-        else
+        else if(!stall)
             ex_mem <= ex_mem_next;
-
     end
-    
+
 endmodule
 `endif
