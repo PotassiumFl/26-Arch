3e06d9e365c5211e63da465a7a41d8539dd88610
顾开孚 <3390945494@qq.com>
2026-03-14 13:22:13 +0800

    revise regfile connect diffset reg

--- vsrc/src/RegFile.sv
+++ vsrc/src/RegFile.sv
@@ -6,25 +6,41 @@
 `endif
 
 module RegFile import common::*; (
-    input logic             clk,reset,
-    input RegFile_ctrl_t    RegFile_ctrl,
-    output i64              rs1_data,rs2_data
+    input  logic            clk,reset,
+    input  RegFile_read_t   RegFile_read,
+    input  RegFile_write_t  RegFile_write,
+    output i64              rs1_data,rs2_data,
+    output i64              reg_c[0:31]
 );
 
-    i64 [31:0] regFile;
+    i64 regFile [31:0];
 
-    always_ff @(posedge clk or posedge reset) begin
-        if(reset) begin
-            for(integer i = 0;i<32;i=i+1)
-                regFile[i]<=64'b0; 
-        end
-        else if(RegFile_ctrl.w_en && RegFile_ctrl.wd!=0) begin
-            regFile[RegFile_ctrl.wd]<=RegFile_ctrl.write_data;
+    always_comb begin
+        for (int i = 0; i < 32; i++) begin
+            if (i == 0) begin
+                reg_c[i]='0;
+            end else if (RegFile_write.w_en && (i[4:0] == RegFile_write.wd)) begin
+                reg_c[i[4:0]] = RegFile_write.w_data; 
+            end else begin
+                reg_c[i[4:0]] = regFile[i[4:0]]; 
+            end
         end
     end
 
-    assign rs1_data = RegFile_ctrl.rs1==0?64'b0:regFile[RegFile_ctrl.rs1];
-    assign rs2_data = RegFile_ctrl.rs2==0?64'b0:regFile[RegFile_ctrl.rs2];
+    always_ff @(posedge clk or posedge reset) begin : rst_write
+        if (reset) begin
+		for (int i = 0; i < 32; i++) begin
+			regFile[i[4:0]] <= 64'b0;
+		end
+	end else begin
+		for (int i = 0; i < 32; i++) begin
+			regFile[i[4:0]] <= reg_c[i[4:0]];
+		end
+	end
+    end
+
+    assign rs1_data = RegFile_read.rs1 == 0 ? 64'b0 : regFile[RegFile_read.rs1];
+    assign rs2_data = RegFile_read.rs2 == 0 ? 64'b0 : regFile[RegFile_read.rs2];
     
 endmodule
 
