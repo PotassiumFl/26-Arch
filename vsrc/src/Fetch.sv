a1cdf78a98168d37382e4c09eb11fe684a60daf1
顾开孚 <3390945494@qq.com>
2026-03-14 13:21:00 +0800

    revise fetch

--- vsrc/src/Fetch.sv
+++ vsrc/src/Fetch.sv
@@ -6,51 +6,59 @@
 `endif
 
 module Fetch import common::*;(
-    input   logic           clk,reset,
+    input  logic       clk, reset,
+    input  logic       stall,
 
-    input   ibus_resp_t     iresp,
-    output  ibus_req_t      ireq,
+    input  ibus_resp_t iresp,
+    output ibus_req_t  ireq,
 
-    output  IF_ID_t         if_id
+    output IF_ID_t     if_id
 );
 
-    u64           pc;
-    fetch_state_t state;
-    
+    u64 pc;
+    u32 instr;
 
-    always_ff @( posedge clk or posedge reset) begin : fetch
-        if (reset) begin
-            pc          <= PCINIT;
-            state       <= IDLE;
-            ireq.valid  <= 0;
-            if_id.valid <= 0;
+    logic waiting;
+    logic instr_valid;
+
+    IF_ID_t if_id_next;
+
+    assign ireq.valid = waiting;
+    assign ireq.addr  = pc;
+
+    always_ff @(posedge clk or posedge reset) begin
+        if(reset) begin
+            pc <= PCINIT;
+            waiting <= 1'b1;
+            instr_valid <= 1'b0;
         end
         else begin
-            case (state)
-                IDLE: begin
-                    ireq.valid <= 1;
-                    ireq.addr  <= pc;
-                    state      <= WAIT_ADDR;
-                end 
-
-                WAIT_ADDR:
-                    if (iresp.addr_ok) begin
-                        if_id.valid <= 1;
-                        state       <= WAIT_DATA;
-                    end
-
-                WAIT_DATA:
-                    if (iresp.data_ok) begin
-                        if_id.valid <= 1;
-                        if_id.decoder_ctrl.instr <= iresp.data;
-                        if_id.decoder_ctrl.pc <= pc;
-
-                        pc <= pc + 4;
-                        state <= IDLE;
-                    end
-                default: state <= state;
-            endcase
+            if(waiting) begin
+                if(iresp.data_ok) begin
+                    instr <= iresp.data;
+                    instr_valid <= 1'b1;
+                    waiting <= 1'b0;
+                end
+            end
+            else if(!stall) begin
+                pc <= pc + 4;
+                waiting <= 1'b1;
+                instr_valid <= 1'b0;
+            end
         end
+    end
+
+    always_comb begin
+        if_id_next.valid = instr_valid;
+        if_id_next.decoder_ctrl.instr = instr;
+        if_id_next.decoder_ctrl.pc    = pc;
+    end
+
+    always_ff @(posedge clk or posedge reset) begin
+        if(reset)
+            if_id <= '0;
+        else if(!stall)
+            if_id <= if_id_next;
     end
 
 endmodule
