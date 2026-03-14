5c8619a40d63be9175380b63206000cf596020f6
顾开孚 <3390945494@qq.com>
2026-03-14 13:22:02 +0800

    writeback stage

---
+++ vsrc/src/Wb.sv
@@ -0,0 +1,19 @@
+`ifndef __WB_SV
+`define __WB_SV
+
+`ifdef VERILATOR
+`include "include/common.sv"
+`endif
+
+module Wb import common::*; (
+    input MEM_WB_t mem_wb,
+    output RegFile_write_t RegFile_write
+);
+
+    assign RegFile_write.wd = mem_wb.wd;
+    assign RegFile_write.w_data = mem_wb.result;
+    assign RegFile_write.w_en = mem_wb.reg_write;
+    
+endmodule
+
+`endif
